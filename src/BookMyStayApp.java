99cdcdc0a547935002ab13204ae1e8016baaaa5c
nithish84385 <nithishsrvtrycbseix@gmail.com>
2026-03-10 15:13:09 +0530

    Use Case 1 - Application entry point

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,24 +1,11 @@
-
-
-/**
- * BookMyStayApp
- *
- * Entry point for the Hotel Booking Management System.
- * Demonstrates how a Java application starts execution
- * and prints a welcome message to the console.
- *
- * @author Nithish DR
- * @version 1.0
- */
-
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to BookMyStayApp ");
-        System.out.println(" Hotel Booking System v1.0 ");
-        System.out.println("=================================");
+        System.out.println("====================================");
+        System.out.println("Welcome to BookMyStayApp");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("====================================");
 
         System.out.println("Application started successfully.");
         System.out.println("Application terminated.");
