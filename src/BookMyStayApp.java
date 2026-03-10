58092f532122376ebda5298127885b109bc3af2c
nithish84385 <nithishsrvtrycbseix@gmail.com>
2026-03-10 15:20:03 +0530

    Use Case 3 - Centralized inventory management using HashMap

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,26 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
 
+class RoomInventory {
 
-/**
- * BookMyStayApp
- *
- * Entry point for the Hotel Booking Management System.
- * Demonstrates how a Java application starts execution
- * and prints a welcome message to the console.
- *
- * @author Nithish DR
- * @version 1.0
- */
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    public void updateAvailability(String roomType, int count) {
+        inventory.put(roomType, count);
+    }
+
+    public void displayInventory() {
+        System.out.println("Current Room Inventory");
+        System.out.println("======================");
+
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue() + " rooms available");
+        }
+    }
+}
 
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to BookMyStayApp ");
-        System.out.println(" Hotel Booking System v1.0 ");
-        System.out.println("=================================");
+        RoomInventory inventory = new RoomInventory();
 
-        System.out.println("Application started successfully.");
-        System.out.println("Application terminated.");
+        inventory.displayInventory();
+
+        System.out.println();
+
+        inventory.updateAvailability("Single Room", 4);
+
+        System.out.println("After Updating Availability");
+        inventory.displayInventory();
     }
 }
